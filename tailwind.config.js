159307544a0d2bd7c11a133cf693203d4c47cb58
atrin-ay <calledatrin@gmail.com>
2024-08-16 04:58:28 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'], // حذف CSS های غیر ضروری در تولید
+  darkMode: false, // یا 'media' یا 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
